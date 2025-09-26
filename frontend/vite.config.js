d58364b4df53fef8859b7a4f682a95ec50c09c9b
Pedro Henrique <pedroheoc@gmail.com>
2025-09-26 09:17:32 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
             target: "http://localhost:5000"
         }
     }
-  }
+  },
+   build: {
+    // Define o nome da pasta de saída
+    outDir: 'dist' 
+  },
 
 })
