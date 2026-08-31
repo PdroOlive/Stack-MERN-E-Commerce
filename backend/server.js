9cf1c4ce1c3c3f36f15e8cbd1ba227aa678e8832
Pedro Henrique <pedroheoc@gmail.com>
2026-08-31 10:21:33 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist/index.html")));
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
     app.get("*", (res, req) => {
         res.sendfile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 
 }
 
-app.listen(5000, () => {
+app.listen(port, () => {
     connectDB();
     console.log(`Server Started at ${baseUrl}:${port}`);
 })
